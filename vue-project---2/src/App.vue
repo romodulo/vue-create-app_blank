3fce0a911248f017c908308fd6775006ba1fd343
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-03-26 23:59:08 +1000

    First commit of < 3-26>  <23:58>  :  add vue-project---2 as reference

---
+++ vue-project---2/src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <div>Hello</div> -->
+  </header>
+  <div>New section</div>
+  <div>End:New secion</div>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  place-items: center;
+  line-height: 1.5;
+  background-color: coral;
+}
+
+header {
+  border-radius: 50px;
+}
+
+.logo {
+  /* display: block; */
+  margin: 0.71rem auto;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    background-color: burlywood;
+  }
+
+  .logo {
+    /* margin: 0 2rem; */
+    /* below: */
+    /* to make it look like width less-than 1024 */
+    margin: 0 auto;
+    /* note */
+    /* :what makes it different is coming from another css-stylesheet: */
+    /* :most likely base.css */
+    /* :correction: it is main.css instead */
+    /* :under the css-selector of id:app, */
+    /* that is what is happening */
+    
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ vue-project---2/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+  superMsg : {
+    type: String,
+    required: true,
+  },
+})
+
+const link_1 = {"link" :"https://vite.dev", "label" :"Vite_modified!!!"}
+const link_2 = {"link" :"https://vuejs.org", "label" :"Vue-3_Modified"}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}<br><span style="padding-top: 10px;">{{ superMsg }}</span></h1>
+    <h3>
+      You’ve successfully created a project with
+      <a v-bind:href="link_1.link" target="_blank" rel="noopener">{{ link_1.label }}</a> +
+      <a v-bind:href="link_2.link" target="_blank" rel="noopener">{{ link_2.label }}</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
